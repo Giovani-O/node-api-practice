0b4f289caa155dc126bb1198be380097a9a0036f
Giovani <giovani98@proton.me>
2026-04-08 16:57:39 -0300

    feat: Add cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -15,12 +15,52 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7,
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
-      title
+      title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
+  })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
@@ -8,6 +9,7 @@ app.get('/health', async () => {
   return `\nHTTP server running!\n\n`
 })
 
+app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
